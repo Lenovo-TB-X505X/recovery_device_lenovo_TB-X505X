61bb6ab1c8b5b7f721c8abe45f59eed5d59a1e25
ViaanLarryROMS <viaan152013@gmail.com>
2026-03-22 03:00:14 +0000

    Initial TWRP device tree for Lenovo TB-X505X (twrpdtgen base)

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_TB-X505X.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_TB-X505X-user \
+    omni_TB-X505X-userdebug \
+    omni_TB-X505X-eng
---
+++ README.md
@@ -0,0 +1,10 @@
+# Android device tree for Lenovo Lenovo TB-X505X (TB-X505X)
+
+```
+#
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
