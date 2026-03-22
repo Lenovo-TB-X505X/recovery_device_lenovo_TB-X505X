cdd4f78ee005829e82540302ccb76ba69f4f59c2
ViaanROM <viaan152013@gmail.com>
2026-03-22 09:51:00 +0530

    TB-X505X: recovery: Include ABOV CapSense touch firmware
    The ABOV touch controller on the TB-X505X requires a specific 
    firmware binary (AbovCapSense.BIN) to initialize correctly. 
    Without this, the kernel registers the input devices but fails 
    to process touch events (Error -11 in dmesg).
    
    - Added AbovCapSense.BIN to prebuilts
    - Fixed "could not find firmware" ueventd error
    - Enables touch functionality in TWRP

--- device.mk
+++ device.mk
@@ -6,3 +6,7 @@
 #
 
 LOCAL_PATH := device/lenovo/TB-X505X
+
+# Touch Firmware
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/vendor/firmware/AbovCapSense.BIN:recovery/root/vendor/firmware/AbovCapSense.BIN
